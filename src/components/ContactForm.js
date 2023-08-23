400bed9433635b0993246ebc59da07cf3c79e317
cting0304 <christian.ting0304@gmail.com>
2023-08-24 02:18:55 +0800

    Completed Part 3

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { addContact } from '../redux/contactActions';
+import '../App.css';
 
-function ContactForm({ addContact }) {
-  const [contact, setContact] = useState({
+const ContactForm = ({ addContact }) => {
+  const [formData, setFormData] = useState({
     firstName: '',
     middleName: '',
     lastName: '',
@@ -11,25 +12,89 @@ function ContactForm({ addContact }) {
     emailAddress: '',
   });
 
-  const handleChange = (e) => {
+  const [errors, setErrors] = useState({
+    firstName: '',
+    lastName: '',
+    mobileNumber: '',
+    emailAddress: '',
+  });
+
+  const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setContact((prevContact) => ({
-      ...prevContact,
+    let newErrors = { ...errors };
+    
+    if (name === 'firstName' || name === 'lastName') {
+      newErrors[name] = value.trim() === '' ? 'This field is required.' : '';
+    } else if (name === 'mobileNumber') {
+      newErrors[name] = !isValidMobileNumber(value) ? 'Please enter a valid number.' : '';
+    } else if (name === 'emailAddress') {
+      newErrors[name] = !isValidEmailAddress(value) ? 'Please enter a valid email address.' : '';
+    }
+
+    setErrors(newErrors);
+
+    setFormData((prevData) => ({
+      ...prevData,
       [name]: value,
     }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    addContact(contact);
-    // Clear the form after adding the contact
-    setContact({
-      firstName: '',
-      middleName: '',
-      lastName: '',
-      mobileNumber: '',
-      emailAddress: '',
-    });
+
+    if (validateForm()) {
+      // Dispatch an action to add the contact using Redux
+      addContact(formData);
+
+      // Clear the form fields
+      setFormData({
+        firstName: '',
+        middleName: '',
+        lastName: '',
+        mobileNumber: '',
+        emailAddress: '',
+      });
+
+      setErrors({
+        firstName: '',
+        middleName: '',
+        lastName: '',
+        mobileNumber: '',
+        emailAddress: '',
+      });
+    }
+  };
+  const isValidMobileNumber = (value) => {
+    // Validation for mobile number
+    return /^[0-9]{11}$/.test(value);
+  };
+
+  const isValidEmailAddress = (value) => {
+    // Validation for email address
+    const atIndex = value.indexOf('@');
+    const dotIndex = value.lastIndexOf('.');
+    return atIndex > 0 && dotIndex > atIndex + 1 && dotIndex < value.length - 1;
+  };
+
+  const validateForm = () => {
+    let formValid = true;
+    const newErrors = { ...errors };
+
+    
+    for (const field of ['firstName', 'lastName', 'mobileNumber', 'emailAddress']) {
+      if (formData[field].trim() === '') {
+        newErrors[field] = 'This field is required.';
+        formValid = false;
+      }
+    }
+
+    if (formData.mobileNumber.trim() !== '' && formData.mobileNumber.length !== 11) {
+      newErrors['mobileNumber'] = 'Mobile number must be 11 digits.';
+      formValid = false;
+    }
+
+    setErrors(newErrors);
+    return formValid;
   };
 
   return (
@@ -40,7 +105,10 @@ function ContactForm({ addContact }) {
           id="firstName"
           name="firstName"
           placeholder="First Name"
+          value={formData.firstName}
+          onChange={handleInputChange}
         />
+        <div className="error">{errors.firstName}</div>
       </div>
       <div>
         <input
@@ -48,6 +116,8 @@ function ContactForm({ addContact }) {
           id="middleName"
           name="middleName"
           placeholder="Middle Name"
+          value={formData.middleName}
+          onChange={handleInputChange}
         />
       </div>
       <div>
@@ -56,7 +126,10 @@ function ContactForm({ addContact }) {
           id="lastName"
           name="lastName"
           placeholder="Last Name"
+          value={formData.lastName}
+          onChange={handleInputChange}
         />
+        <div className="error">{errors.lastName}</div>
       </div>
       <div>
         <input
@@ -64,7 +137,10 @@ function ContactForm({ addContact }) {
           id="mobileNumber"
           name="mobileNumber"
           placeholder="Mobile Number"
+          value={formData.mobileNumber}
+          onChange={handleInputChange}
         />
+        <div className="error">{errors.mobileNumber}</div>
       </div>
       <div>
         <input
@@ -72,12 +148,16 @@ function ContactForm({ addContact }) {
           id="emailAddress"
           name="emailAddress"
           placeholder="Email Address"
+          value={formData.emailAddress}
+          onChange={handleInputChange}
         />
+        <div className="error">{errors.emailAddress}</div>
       </div>
       <button type="submit">Add Contact</button>
     </form>
   );
-}
+};
+
 
 const mapDispatchToProps = {
   addContact,
